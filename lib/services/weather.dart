9098a30881d811dff9be16e10f461d2bc1a16116
Andres Melenchon <andres.melenchon@finetwork.com>
2020-12-01 11:40:40 +0100

    Finished Clima App Bootcamp Version

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,32 @@
+import 'package:clima/services/networking.dart';
+import 'package:clima/location.dart';
+
+const apiKey = 'd2c47fb90279384a63e52adfe52136e4';
+const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
+
 class WeatherModel {
+
+  Future<dynamic> getCityWeather(String cityName) async{
+
+    var url = '$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric&lang=sp';
+
+    NetworkHelper networkHelper = NetworkHelper(url: url);
+    var weatherData = networkHelper.getData();
+    return weatherData;
+  }
+
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    NetworkHelper networkHelper = NetworkHelper(
+        url:
+            '$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric&lang=sp');
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
